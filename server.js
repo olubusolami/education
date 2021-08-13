c34d0c6456e588d3e94110bb2b940e39c1829247
olubusolami <olubusolao3@gmail.com>
2021-08-13 21:32:03 +0100

    cors configuration

--- server.js
+++ server.js
@@ -28,6 +28,45 @@ const { loginUser } = require("./routes/auth");
 
 dotenv.config();
 
+//connect to mongoose
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((res) => console.log("connected to the database"));
+
+app.use(express.json());
+
+//configure cors
+app.use(cors());
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Authorization"
+  );
+
+  // Set to true if you need the website to include
+  //cookies in the requests sent to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello and Welcome 🙌");
 });
@@ -186,43 +225,6 @@ app.delete("/lithuaina/:id", auth, function (req, res) {
         .json({ status: "Success", message: "Deleted Successfully" });
     })
     .catch((error) => next(err));
-});
-
-//connect to mongoose
-mongoose
-  .connect(process.env.DB_CONNECT, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then((res) => console.log("connected to the database"));
-
-app.use(express.json());
-
-app.use(cors());
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
 });
 
 //middleware
