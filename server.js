98a638144e9deee30ff7557e16cd6cbcac55a853
gtstride <lewisagho47@gmail.com>
2021-08-16 20:39:49 +0100

    add limit of 50mb for file upload express

--- server.js
+++ server.js
@@ -41,8 +41,12 @@ mongoose
 app.use(cors());
 
 //express passage
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+// app.use(express.json());
+// app.use(express.urlencoded({ limit:'50mb', extended: true }));
+
+
 
 
 app.use(function (req, res, next) {
